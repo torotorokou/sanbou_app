7cd96d50f6e08f84fa91f1f55ff56b95ac360462
torotoro630 <happy.go.lucky.ssd@gmail.com>
2025-07-02 16:46:36 +0900

    ref_帳票

---
+++ my-project/my-dashboard-app/src/pages/report/ReportAdminSheet.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ReportAdminSheet: React.FC = () => {
+    return <div>🗂️ 管理票ページ</div>;
+};
+
+export default ReportAdminSheet;
---
+++ my-project/my-dashboard-app/src/pages/report/ReportPrice.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ReportPrice: React.FC = () => {
+    return <div>💰 ブロック単価表ページ</div>;
+};
+
+export default ReportPrice;
