14ab9db4fcf2737ffce1b652c53750926ae4ef93
torotoro630 <happy.go.lucky.ssd@gmail.com>
2025-11-04 09:05:51 +0900

    before_inbound_card

---
+++ app/backend/core_api/migrations/alembic/versions/4befa8148602_msg.py
@@ -0,0 +1,24 @@
+"""msg
+
+Revision ID: 4befa8148602
+Revises: 002
+Create Date: 2025-10-31 08:37:21.523472
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4befa8148602'
+down_revision = '002'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
