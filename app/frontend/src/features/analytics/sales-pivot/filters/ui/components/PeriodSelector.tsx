c98e6747c07512dabac9918a786678769149cd20
torotoro630 <happy.go.lucky.ssd@gmail.com>
2025-12-01 14:27:12 +0900

    fix: dayjs updateLocaleプラグインをインポート
    - dayjs/plugin/updateLocaleを追加してエラーを解消

--- app/frontend/src/features/analytics/sales-pivot/filters/ui/components/PeriodSelector.tsx
+++ app/frontend/src/features/analytics/sales-pivot/filters/ui/components/PeriodSelector.tsx
@@ -1,9 +1,13 @@
 import { Space, Segmented, DatePicker, Typography } from 'antd';
 import type { Dayjs } from 'dayjs';
 import dayjs from 'dayjs';
+import updateLocale from 'dayjs/plugin/updateLocale';
 import locale from 'antd/es/date-picker/locale/ja_JP';
 import 'dayjs/locale/ja';
 import styles from './PeriodSelector.module.css';
+
+// updateLocaleプラグインを有効化
+dayjs.extend(updateLocale);
 
 // dayjsのロケールを日本語に設定し、月曜始まりに変更
 dayjs.locale('ja');
